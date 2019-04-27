0a80ab61b3c3aeb9f3f5cc669b4bee3d0f08a909
wtecharut <techarut@gmail.com>
2019-04-27 16:45:05 -0400

    Update EnvInit_Debian01.sh

--- EnvInit_Debian01.sh
+++ EnvInit_Debian01.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 # My first script
 
+#NodeJs
 sudo apt-get update
+sudo apt install nodejs npm
+sudo apt install npm
+
+# NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/.bashrc
+nvm install v10.15.3
+nvm alias default v10.15.3 #changing default
+
+#Truffle
+nvm install v10.15.3
+npm install truffle -g
